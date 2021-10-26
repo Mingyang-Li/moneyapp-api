b2d4a9b31493efe08d4b704e718c553bf1a83b1d
Mingyang-Li <53138432+Mingyang-Li@users.noreply.github.com>
2021-10-27 11:34:36 +1300

    Inserted auth middleware

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthenticationMiddleware } from '@common/authentication.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): MiddlewareConsumer | void {
+    consumer
+      .apply(AuthenticationMiddleware)
+      .forRoutes(
+        { method: RequestMethod.GET, path: '/api/get' },
+        { method: RequestMethod.POST, path: '/api/post' },
+        { method: RequestMethod.PUT, path: '/api/edit' },
+        { method: RequestMethod.DELETE, path: '/api/delete' },
+      );
+  }
+}
