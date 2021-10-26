a22c09ef620d31058c2c9e5dbc6af99f508e56c3
Mingyang-Li <53138432+Mingyang-Li@users.noreply.github.com>
2021-10-27 12:26:00 +1300

    Enable swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const options = new DocumentBuilder()
+    .setTitle('Money App API')
+    .setDescription('Personal use ')
+    .setVersion('1.0.0')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'Token' },
+      'access-token',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(4000);
+
+  console.log('===============================');
+  console.log(`= 🚀 App running on port ${4000} =`);
+  console.log('===============================');
 }
 bootstrap();
