1791ccd141804ccdee2dfdfc16cc215116c04f62
Mingyang-Li <53138432+Mingyang-Li@users.noreply.github.com>
2021-10-29 00:20:18 +1300

    Made notion client work

--- src/notion/notion.service.ts
+++ src/notion/notion.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { IncomeRow } from './notion.entity';
+import { Client } from '@notionhq/client';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: __dirname + '/.env' });
 
 @Injectable()
 export class NotionService {
+  private notionClient = new Client({ auth: process.env.NOTION_TOKEN });
+
   async findAllIncome(): Promise<IncomeRow[]> {
     const incomeRow = new IncomeRow();
     incomeRow.id = 'test1';
+    const response = await this.notionClient.databases.query({
+      database_id: process.env.NOTION_INCOME_DATABASE_ID,
+    });
+    response.results.forEach((row) => {
+      console.table(row.properties);
+    });
     return [incomeRow];
   }
 }
