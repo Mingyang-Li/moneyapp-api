8bab20565c5fb853a3ffc15d03e6a730e4d6a58e
Mingyang-Li <53138432+Mingyang-Li@users.noreply.github.com>
2021-11-19 22:14:11 +1300

    Blocked requests without payloads

--- src/auth/gql-auth0.guard.ts
+++ src/auth/gql-auth0.guard.ts
@@ -6,36 +6,43 @@ import {
   HttpStatus,
 } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
-import { JwtService } from '@nestjs/jwt';
+import jwt from 'jsonwebtoken';
 import * as dotenv from 'dotenv';
 dotenv.config({ path: __dirname + '/.env' });
 
 @Injectable()
 export class GqlAuth0Guard implements CanActivate {
-  constructor(private readonly jwtService: JwtService) {}
+  // constructor(private readonly jwtService: JwtService) {}
   public canActivate(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context).getContext();
-    // Auth checks:
-    // 1. If no auth header => UNAUTHENTICATED
-    // 2. If JWT token is malformed or invalid => UNAUTHENTICATED
+    // Auth scenario checks:
+    // 1. If no auth header => UNAUTHENTICATED (done)
+    // 2. If JWT token is malformed or invalid => UNAUTHENTICATED (in progress)
     // 3. If token has valid fornat BUT any of extracted audience OR issuer does not equal to ones specified in env => UNAUTHENTICATED
     // 4. If token has valid fornat BUT permissions scope from decoded JWT token doesn't have admin scopes => UNAUTHORISED
     const authHeader = ctx.headers.authorization;
+
+    // Scenario 1 check
     if (!authHeader) {
       return false;
     }
+
+    // Scenario 2, 3, 4 checks
     ctx.user = this.validateToken(authHeader);
     return true;
   }
 
   protected async validateToken(auth: string) {
-    if (auth.split(' ')[0] !== 'Bearer') {
+    const token = auth.split(' ')[1];
+    // const verified = jwt.verify(token, 'test');
+    // console.log(`Verified: ${verified}`);
+    if (!token) {
       throw new HttpException('Invalid token', HttpStatus.UNAUTHORIZED);
     }
-    const token = auth.split(' ')[1];
+    // const token = auth.split(' ')[1];
     try {
-      const decoded = this.jwtService.verify(token);
-      return decoded;
+      // const decoded = this.jwtService.verify(token);
+      return true;
     } catch (err) {
       const message = 'Token error: ' + (err.message || err.name);
       throw new HttpException(message, HttpStatus.UNAUTHORIZED);
--- src/auth/gql-auth0.module.ts
+++ src/auth/gql-auth0.module.ts
@@ -1,9 +1,6 @@
 import { Module } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  imports: [JwtService],
-  providers: [JwtService],
   exports: [GqlAuth0Module],
 })
 export class GqlAuth0Module {}
