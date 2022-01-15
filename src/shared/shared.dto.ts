31198aac75dedb71cd67bc5274829c402fe2c0e4
Mingyang-Li <mingyangli118@gmail.com>
2022-01-15 17:43:47 +1300

    Init shared dtos

--- src/shared/shared.dto.ts
+++ src/shared/shared.dto.ts
@@ -1,1 +1,30 @@
-export const a = 0;
+export interface BaseQueryParam {
+  id?: number;
+  notionId?: string;
+  amount?: number;
+  currency?: string;
+  date?: Date;
+  dateCreated?: Date;
+  dateLastUpdated?: Date;
+  dateDeleted?: Date;
+  deleted?: boolean;
+
+  // functionAttributes
+  sortDateBy?: OrderByType;
+  count?: number;
+}
+
+export interface BaseGroupingType {
+  currency?: 'currency';
+  deleted?: 'deleted';
+}
+
+export type OrderByType = 'asc' | 'desc';
+
+export interface DbCount {
+  _count: Field;
+}
+
+export interface Field {
+  notionId?: number;
+}
