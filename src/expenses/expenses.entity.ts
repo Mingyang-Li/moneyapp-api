9c860e1f422ef6d42bb9d3db371e07b09dc4e6c7
Mingyang-Li <mingyangli118@gmail.com>
2022-01-15 17:52:34 +1300

    Init entities for new expenses module

--- src/expenses/expenses.entity.ts
+++ src/expenses/expenses.entity.ts
@@ -1,1 +1,40 @@
-export const a = 0;
+import {
+  BaseAverage,
+  BaseGroupByQueryReturnedFields,
+  BaseIncomeAndExpenseRow,
+} from '@/shared/shared.entity';
+import { ObjectType, Field } from '@nestjs/graphql';
+
+@ObjectType()
+export class ExpenseRow extends BaseIncomeAndExpenseRow {
+  @Field({ nullable: true })
+  item?: string;
+
+  @Field({ nullable: true })
+  type?: string;
+
+  @Field({ nullable: true })
+  subType?: string;
+
+  @Field({ nullable: true })
+  paymentType?: string;
+}
+
+@ObjectType()
+export class ExpenseGroupByQuery extends BaseGroupByQueryReturnedFields {
+  // expense_specific
+  @Field({ nullable: true })
+  expenseType?: string;
+
+  @Field({ nullable: true })
+  expenseSubType?: string;
+
+  @Field({ nullable: true })
+  expensePaymentType?: string;
+}
+
+@ObjectType()
+export class AverageExpenses extends BaseAverage {
+  @Field()
+  average: number;
+}
